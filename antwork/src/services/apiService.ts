8a00ea1b7d0591e95d71fe413a6e76abdc35adec
nwejin <chlwls4m@gmail.com>
2024-01-23 03:14:36 +0900

    1/23-2

--- antwork/src/services/apiService.ts
+++ antwork/src/services/apiService.ts
@@ -93,6 +93,7 @@ export const FindId = async (userData: any) => {
         );
         return response.data;
     } catch (error) {
+        console.error(error);
         throw new Error("예상치 못한 오류가 발생했습니다!");
     }
 };
@@ -137,9 +138,10 @@ export const ChangePw = async (userData: any) => {
 // 커뮤니티 게시글 생성
 export const newPost = async (communityData: any) => {
     console.log('communityData>', communityData );
+    console.log('Sending POST request to /community/write');
     try {
         const response = await axios.post(
-            process.env.REACT_APP_BACKSERVER +"/community",
+            process.env.REACT_APP_BACKSERVER + "/community/write",
             communityData,{
               headers: {
                  'Content-Type': 'application/json',
@@ -155,3 +157,14 @@ export const newPost = async (communityData: any) => {
 };
 
 // 커뮤니티 게시글 가져오기 (커뮤니티 메인 화면에 띄우기)
+export const getCommunityPosts = async () => {
+    try {
+        const response = await axios.get(
+            process.env.REACT_APP_BACKSERVER + "/community/read"
+        );
+        return response.data;
+    } catch (error) {
+        console.error(error); // 에러 메시지를 콘솔에 출력
+        throw new Error("예상치 못한 오류가 발생했습니다!");
+    }
+};
