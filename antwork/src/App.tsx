5e2e50cc975dcd54ec378143f1adeb474161d998
nwejin <chlwls4m@gmail.com>
2024-01-24 13:58:21 +0900

    1/24

--- antwork/src/App.tsx
+++ antwork/src/App.tsx
@@ -1,81 +1,77 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./styles/Component.scss";
-import "./styles/Header.scss";
-import "./styles/Signin.scss";
-import "./styles/Main.scss";
-import "./styles/StockGuide.scss";
-import Header from "./components/Header";
-import MainPage from "./pages/MainPage";
-import SigninPage from "./pages/SigninPage";
-import SignupPage from "./pages/SignupPage";
-import FindIdPage from "./pages/FindIdPage";
-import CommunityMain from "./pages/community/CommunityMain";
-import CommunityReadPage from "./pages/community/CommunityReadPage";
-import axios from "axios";
-import NewsPage from "./pages/NewsPage";
-import NewsDetailPage from "./pages/NewsDetailPage";
-import StockGuidePage from "./pages/StockGuidePage";
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './styles/Component.scss';
+import './styles/Header.scss';
+import './styles/Signin.scss';
+import './styles/Main.scss';
+import './styles/StockGuide.scss';
+import Header from './components/Header';
+import MainPage from './pages/MainPage';
+import SigninPage from './pages/SigninPage';
+import SignupPage from './pages/SignupPage';
+import FindIdPage from './pages/FindIdPage';
+import CommunityMain from './pages/community/CommunityMain';
+import CommunityReadPage from './pages/community/CommunityReadPage';
+import axios from 'axios';
+import NewsPage from './pages/NewsPage';
+import NewsDetailPage from './pages/NewsDetailPage';
+import StockGuidePage from './pages/StockGuidePage';
 // import ExampleComponent from "./components/ExampleComponent";
 
-
 function App() {
-    const [serverData, setServerData] = useState("");
-    useEffect(() => {
-        // React 컴포넌트가 마운트될 때 한 번 실행
-        fetchDataFromServer();
-    }, []);
+  const [serverData, setServerData] = useState('');
+  useEffect(() => {
+    // React 컴포넌트가 마운트될 때 한 번 실행
+    fetchDataFromServer();
+  }, []);
 
-    const fetchDataFromServer = async () => {
-        if (process.env.REACT_APP_BACKSERVER) {
-            try {
-                const response = await axios.get(process.env.REACT_APP_BACKSERVER!, {
-                    withCredentials: true, // axios에서는 credentials를 설정할 때 withCredentials를 사용
-                    headers: {
-                        'Content-Type': 'application/json',
-                        // 필요에 따라 다른 헤더를 추가할 수 있음
-                    },
-                });
-        
-                console.log(response.data);
-                setServerData(response.data.message);
-            } catch (error) {
-                console.error("Error fetching data with axios:", error);
-            }
-        } else {
-            return;
-        }
-    };
+  const fetchDataFromServer = async () => {
+    if (process.env.REACT_APP_BACKSERVER) {
+      try {
+        const response = await axios.get(process.env.REACT_APP_BACKSERVER!, {
+          withCredentials: true, // axios에서는 credentials를 설정할 때 withCredentials를 사용
+          headers: {
+            'Content-Type': 'application/json',
+            // 필요에 따라 다른 헤더를 추가할 수 있음
+          },
+        });
 
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<MainPage/>} />
-                    <Route path="/news/all" element={<NewsPage />} />
-                    <Route path="/news/economy" element={<NewsPage />} />
-                    <Route path="/news/stock" element={<NewsPage />} />
-                    <Route path="/news/coin" element={<NewsPage />} />
+        console.log(response.data);
+        setServerData(response.data.message);
+      } catch (error) {
+        console.error('Error fetching data with axios:', error);
+      }
+    } else {
+      return;
+    }
+  };
 
-                    <Route path="/signin" element={<SigninPage />} />
-                    <Route path="/signup" element={<SignupPage />} />
-                    <Route path="/findId" element={<FindIdPage />} />
-                    <Route path="/news/:id" element={<NewsDetailPage />} />
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/news/all" element={<NewsPage />} />
+          <Route path="/news/economy" element={<NewsPage />} />
+          <Route path="/news/stock" element={<NewsPage />} />
+          <Route path="/news/coin" element={<NewsPage />} />
 
-                    <Route path="/stockGuide" element={<StockGuidePage />} />
-                    <Route path="/community" element={<CommunityMain />} />
-                    <Route
-                        path="/community/read"
-                        element={<CommunityReadPage />}
-                    />
-                </Routes>
-            </BrowserRouter>
-            {/* <ExampleComponent></ExampleComponent> */}
-            {/* <div className="class">{serverData}</div> */}
-            <div>{serverData}</div>
-        </div>
-    );
+          <Route path="/signin" element={<SigninPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/findId" element={<FindIdPage />} />
+          <Route path="/news/:id" element={<NewsDetailPage />} />
+
+          <Route path="/stockGuide" element={<StockGuidePage />} />
+          <Route path="/community" element={<CommunityMain />} />
+          <Route path="/community/read" element={<CommunityReadPage />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <ExampleComponent></ExampleComponent> */}
+      {/* <div className="class">{serverData}</div> */}
+      <div>{serverData}</div>
+    </div>
+  );
 }
 
 export default App;
--- antwork/src/components/community/PostRankList.tsx
+++ antwork/src/components/community/PostRankList.tsx
@@ -1,24 +1,57 @@
 import '../../styles/Community.scss';
+import React, { useState, useEffect } from 'react';
+import { getCommunityPosts } from '../../services/apiService';
 
 function PostRankList() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // 서버에서 데이터를 불러와서 posts 상태 업데이트
+    const fetchData = async () => {
+      try {
+        const communityPosts = await getCommunityPosts();
+        setPosts(communityPosts);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
-      <li>
-        <a href="/">
-          <div>
-            <div className="category">
-              <span>경제</span>
-            </div>
-            <div className="listTitle">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </div>
-          </div>
-          <div className="listComment">
-            <span className="material-symbols-outlined">maps_ugc</span>
-            <span>18</span>
-          </div>
-        </a>
-      </li>
+      {posts.map((post: any) => {
+        const getSubject = () => {
+          const subject = post.subject;
+          let subjectname;
+          if (subject === 'free') {
+            subjectname = '자유';
+          } else if (subject === 'economy') {
+            subjectname = '경제';
+          } else if (subject === 'coin') {
+            subjectname = '코인';
+          } else if (subject === 'stock') {
+            subjectname = '주식';
+          }
+          return subjectname;
+        };
+        return (
+          <li>
+            <a href="/">
+              <div>
+                <div className="category">
+                  <span>{getSubject()}</span>
+                </div>
+                <div className="listTitle"> {post.title}</div>
+              </div>
+              <div className="listComment">
+                <span className="material-symbols-outlined">maps_ugc</span>
+                <span></span>
+              </div>
+            </a>
+          </li>
+        );
+      })}
     </>
   );
 }
