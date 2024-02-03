c8e6af40f7cc712ddeb8d8c3688edc4d213785f8
dydgjs200 <dydgjs2016@naver.com>
2024-02-03 16:19:10 +0900

    랭킹 수정

--- src/components/VirtualInvest/showDetail.tsx
+++ src/components/VirtualInvest/showDetail.tsx
@@ -29,8 +29,7 @@ const ShowDetail = ({ response, close, user }: props): ReactElement => {
         const response = await showRank({});
         if (response) {
           console.log('show rank response 전송 성공');
-          console.log('res ', response.rank); // 순위
-          console.log('prfo', response.rank[0].profile);
+          console.log('respone', response);
           // 여기에서 response를 처리하거나 다른 작업을 수행할 수 있습니다.
           setUserRank(response.rank);
         }
